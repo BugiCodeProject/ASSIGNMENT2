b8b93c94c2b839483ef2e8837a9f05f296394a6f
BugiCodeProject <bugitriharso24@gmail.com>
2026-02-21 12:43:14 +0700

    feat:tambah patch dan delete

--- src/modules/todos/router.ts
+++ src/modules/todos/router.ts
@@ -2,16 +2,88 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createTodoSchema } from "./schema.js";
 import { prisma } from "../../utils/prisma.js";
+import { TodoScalarFieldEnum } from "../../generated/prisma/internal/prismaNamespace.js";
 
 export const todoRouter = new Hono()
-.get("/", async(c)=>{
-    const todos = await prisma.todo.findMany()
-    return c.json({todos})
-})
-.get("/:id", (c) => {
-    return c.json({message: "get single review"})
-})
-.post("/", zValidator("json",createTodoSchema),(c) => {
-    const body =  c.req.json()
-    return c.json({ message: "create todos", body})
-})
+  .get("/", async (c) => {
+    try {
+      const todos = await prisma.todo.findMany();
+      console.log(todos);
+      if (!todos) {
+        return c.json({ message: "Not Found" }, 404);
+      }
+      return c.json({ todos });
+    } catch (error) {
+      return c.json({ message: "Terjadi kesalahan pada server" }, 500);
+    }
+  })
+
+  .get("/:id", async (c) => {
+    try {
+      const todoId = Number(c.req.param("id"));
+      const todo = await prisma.todo.findUnique({
+        where: {
+          id: todoId,
+        },
+      });
+      if (!todo) {
+        return c.json({ message: "Not Found" }, 404);
+      }
+      return c.json({ todo });
+    } catch (error) {
+      return c.json({ message: "Terjadi kesalahan pada server" }, 500);
+    }
+  })
+
+  .patch("/:id", async (c) => {
+    try {
+      const todoId = Number(c.req.param("id"));
+      const body = await c.req.json();
+      const updatedTodo = await prisma.todo.update({
+        where: {
+          id: todoId,
+        },
+        data: {
+          title: body.title,
+          content: body.content,
+        },
+      });
+      return c.json(updatedTodo);
+    } catch (error) {
+      return c.json({ message: "Data tidak ditemukan atau gagal diubah" }, 404);
+    }
+  })
+
+  .delete("/:id", async (c) => {
+    try {
+      const todoId = Number(c.req.param("id"));
+
+      const deleteTodo = await prisma.todo.delete({
+        where: {
+          id: todoId,
+        },
+      });
+      return c.json({ message: "Data Berhasil Didelete", data: deleteTodo });
+    } catch (error) {
+      return c.json(
+        { message: "Data tidak ditemukan atau gagal dihapus" },
+        404,
+      );
+    }
+  })
+
+  .post("/", zValidator("json", createTodoSchema), async (c) => {
+    try {
+      const body = c.req.valid("json");
+
+      const newTodo = await prisma.todo.create({
+        data: {
+          title: body.title,
+          content: body.content,
+        },
+      });
+      return c.json(newTodo, 201);
+    } catch (error) {
+      return c.json({ message: "Data gagal di insert" }, 401);
+    }
+  });
